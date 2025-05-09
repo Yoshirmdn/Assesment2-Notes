b8d3cb8013b404b22cef6e6dfcc0a7c4635dba8e
Rio Ragil Ramdani <rioragilramdani7@gmail.com>
2025-05-09 10:35:00 +0700

    refactor: update route key for FormUbah screen to use KEY_ID_NOTES

--- app/src/main/java/com/rioramdani0034/mobpro1/navigation/Screen.kt
+++ app/src/main/java/com/rioramdani0034/mobpro1/navigation/Screen.kt
@@ -1,11 +1,11 @@
 package com.rioramdani0034.mobpro1.navigation
 
-import com.rioramdani0034.mobpro1.ui.screen.KEY_ID_MAHASISWA
+import com.rioramdani0034.mobpro1.ui.screen.KEY_ID_NOTES
 
 sealed class Screen(val route: String){
     data object Home : Screen("mainScreen")
     data object FormBaru : Screen("detailScreen")
-    data object FormUbah : Screen("detailScreen/{$KEY_ID_MAHASISWA}"){
+    data object FormUbah : Screen("detailScreen/{$KEY_ID_NOTES}"){
         fun withId(id: Long) = "detailScreen/$id"
     }
 }
